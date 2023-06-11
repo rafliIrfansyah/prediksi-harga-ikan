260fd995e8655b021464b9031875d62fe03d04c3
rafliIrfansyah <rafli.irfansyah78@gmail.com>
2023-06-12 00:52:10 +0700

    fixed : routes

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ import tensorflow as tf
 
 app = Flask(__name__)
 
+
 model = tf.keras.models.load_model("model.h5")\
 
 from app.routes import prediction
 from app.models import scaler
+
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,2 @@
 from .prediction import *
-from .data import *
 from .home import *
